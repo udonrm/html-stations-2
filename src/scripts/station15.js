11d9a359eec7eb2d98033fc2343ef5b4217551a3
udonrm <b201395@hiroshima-u.ac.jp>
2024-01-05 14:41:53 +0900

    DOM操作で要素の書き換え

---
+++ src/scripts/station15.js
@@ -0,0 +1,11 @@
+const fruits = document.getElementById("fruits");
+
+const displayList = () => {
+  fruits.innerHTML = `
+    <ul>
+        <li>リンゴ</li>
+        <li>イチゴ</li>
+        <li>ブドウ</li>
+    </ul>
+    `;
+};
